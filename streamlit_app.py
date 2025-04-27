bde65cdaa63c2ddfb09625dbae3c84341be63834
Stephen Day <stephen.h.day@gmail.com>
2025-04-27 05:31:04 +0000

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
+
 st.title("🎈 My new apple")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+date = st.date_input("Date?",format="DD-MM-YYYY")
+
+st.write(date.strftime("%d-%b-%Y"))
+
+x = st.multiselect("choose",['a','b','c'],)
+
+st.write(x)
