89a40932a2dad967e691f717a3a5fe4a051cfd97
Stephen Day <stephen.h.day@gmail.com>
2025-05-09 03:23:53 +0000

    chatbot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,42 @@
+import random
+import time
 import streamlit as st
-from datetime import datetime
+from openai import OpenAI
 
-cols = st.columns(2)
-#date = st.date_input("Date?",format="DD-MM-YYYY")
-if not st.session_state.get('go'):
-    cols[0].markdown(f"## {datetime.now()}")
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
-x = cols[1].multiselect("choose",['a','b','c'],)
 
-if cols[1].button("Go!",key='go'):
-    st.write(str(x))
+st.title("Echo Bot")
 
-if st.button('Ballons'):
-    st.balloons()
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if st.button('Snowflakes'):
-    st.snow()
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
