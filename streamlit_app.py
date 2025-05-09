715b8e831d02b88aeb535b5b923da0d84d5d02d1
Stephen Day <stephen.h.day@gmail.com>
2025-05-09 03:37:52 +0000

    new stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import streamlit as st
 def response_generator():
     response = random.choice(
         [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
+            "What's white and falls from the sky?",
+            "Hi, human! Let's play a game.",
+            "What pops at parties?",
         ]
     )
     for word in response.split():
@@ -32,6 +32,11 @@ if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
+    if 'ballon' in prompt:
+        st.balloons()
+
+    if 'snow' in prompt:
+        st.snow()
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
